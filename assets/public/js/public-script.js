fc9380c7f6a9d265c3e4d27c49f6accf68ba0611
Shah Jalal <rjshahjalal132@gmail.com>
2025-02-16 16:10:58 +0600

    made dynamic searching

--- assets/public/js/public-script.js
+++ assets/public/js/public-script.js
@@ -1,7 +1,6 @@
 (function ($) {
   $(document).ready(function () {
-    // Code here
-
+    // start: filter accommodations
     $(".filter-checkbox").on("change", function () {
       // initialize filters
       let filters = [];
@@ -30,5 +29,32 @@
         },
       });
     });
+    // end: filter accommodations
+
+    // start: search accommodations
+    $("#search-accommodation").on("keyup", function () {
+      // get search query
+      let query = $(this).val();
+
+      setTimeout(() => {
+        // AJAX request
+        $.ajax({
+          url: wpb_public_localize.ajax_url,
+          type: "POST",
+          data: {
+            action: "search_accommodations",
+            search: query,
+          },
+          beforeSend: function () {
+            $("#accommodations-wrapper").html("<p>Loading...</p>");
+          },
+          success: function (response) {
+            // render accommodations
+            $("#accommodations-wrapper").html(response);
+          },
+        });
+      }, 500);
+    });
+    // end: search accommodations
   });
 })(jQuery);
--- inc/classes/class-list-accommodations.php
+++ inc/classes/class-list-accommodations.php
@@ -25,8 +25,118 @@ class List_Accommodations {
         add_action( 'wp_ajax_filter_accommodations', [ $this, 'filter_accommodations_callback' ] );
         add_action( 'wp_ajax_nopriv_filter_accommodations', [ $this, 'filter_accommodations_callback' ] );
 
+        // handle search ajax request
+        add_action( 'wp_ajax_search_accommodations', [ $this, 'search_accommodations_callback' ] );
+        add_action( 'wp_ajax_nopriv_search_accommodations', [ $this, 'search_accommodations_callback' ] );
+
         // get all property types (taxonomy)
         // add_action( 'init', [ $this, 'get_property_types' ] );
+
+    }
+
+    public function search_accommodations_callback() {
+        if ( isset( $_POST['search'] ) ) {
+            $search = sanitize_text_field( $_POST['search'] );
+            echo $this->get_searched_accommodations( $search );
+        } else {
+            echo $this->get_searched_accommodations( '' );
+        }
+        wp_die();
+    }
+
+    public function get_searched_accommodations( $search ) {
+        // Default query arguments
+        $args = [
+            "post_type"      => "property",
+            "posts_per_page" => -1,
+            "orderby"        => "title",
+            "order"          => "ASC",
+        ];
+
+        // Set args for filters
+        if ( !empty( $search ) ) {
+            $args["s"] = $search;
+        }
+
+        // get accommodations
+        $properties = new \WP_Query( $args );
+
+        ob_start();
+        ?>
+
+        <div class="row g-4">
+
+            <?php
+            // If there are accommodations, display them
+            if ( $properties->have_posts() ) :
+                while ( $properties->have_posts() ) :
+                    $properties->the_post();
+
+                    // get post id
+                    $post_id = get_the_ID();
+
+                    // get post thumbnail
+                    $_thumbnail            = get_the_post_thumbnail_url( $post_id );
+                    $placeholder_thumbnail = PLUGIN_PUBLIC_ASSETS_URL . '/images/600x400.png';
+                    $thumbnail             = $_thumbnail ? $_thumbnail : $placeholder_thumbnail;
+
+                    // get post permalink
+                    $permalink = get_the_permalink( $post_id );
+
+                    // get post title
+                    $title = get_the_title( $post_id );
+                    // get address
+                    $address = get_post_meta( $post_id, 'Address', true );
+                    // get total guest
+                    $total_guest = get_post_meta( $post_id, 'total_guests', true );
+                    // get total bed
+                    $total_beds = get_post_meta( $post_id, 'bedrooms', true );
+                    // get total bath
+                    $total_baths = get_post_meta( $post_id, 'bathrooms', true );
+                    // get total car
+                    $total_cars = get_post_meta( $post_id, 'car_park', true );
+                    // get price
+                    $price = get_post_meta( $post_id, 'price', true );
+
+                    ?>
+
+                    <div class="col-md-4">
+                        <!-- start: accommodation card -->
+                        <div class="card common-shadow h-515">
+                            <!-- thumbnail image -->
+                            <a href="<?= $permalink ?>"><img src="<?= $thumbnail ?>" class="card-img-top" alt="<?= $title ?>"></a>
+                            <div class="card-body">
+                                <!-- title -->
+                                <h5 class="card-title"><a href="<?= $permalink ?>"
+                                        class="text-dark text-decoration-none"><?= $title ?></a>
+                                </h5>
+                                <!-- address -->
+                                <a href="<?= $permalink ?>" class="mt-3 mb-3"><?= $address ?></a>
+                                <hr class="mt-4">
+                                <!-- room information -->
+                                <p class="mt-3 mb-3 cfs-15"><i class="fa-solid fa-person"></i> <?= $total_guest ?> Guest
+                                    &nbsp; <i class="fa-solid fa-bed"></i> <?= $total_beds ?> Bed
+                                    &nbsp; <i class="fa-solid fa-bath"></i> <?= $total_baths ?> Bath
+                                    &nbsp; <i class="fa-solid fa-car"></i> <?= $total_cars ?> Car
+                                </p>
+                                <hr class="mb-4">
+                                <!-- price -->
+                                <a href="<?= $permalink ?>" class=""><strong>From $<?= $price ?> per
+                                        night</strong></a>
+                            </div>
+                        </div>
+                        <!-- end: accommodation card -->
+                    </div>
+
+                <?php endwhile;
+            else :
+                echo "<p>No accommodations found.</p>";
+            endif;
+
+            ?>
+        </div>
+
+        <?php return ob_get_clean();
 
     }
 
@@ -192,7 +302,8 @@ class List_Accommodations {
                 <div class="col-md-3">
                     <div class="search-bar-section">
                         <h5>Search</h5>
-                        <input type="text" class="form-control mt-1" id="accommodation-search" placeholder="Search accommodations">
+                        <input type="text" class="form-control mt-1" id="search-accommodation"
+                            placeholder="Search accommodations">
                     </div>
                     <div class="filters-items">
                         <?php include_once PLUGIN_BASE_PATH . '/inc/components/filters.php' ?>
